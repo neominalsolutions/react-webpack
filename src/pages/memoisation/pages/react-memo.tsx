ba056af9d8554acd08b39b7674c0eb90d1e92821
neominal.solutions <neominal.solutions@gmail.com>
2025-01-31 09:47:54 +0300

    ReactMemo

--- src/pages/memoisation/pages/react-memo.tsx
+++ src/pages/memoisation/pages/react-memo.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
 
 function Parent() {
+	console.log('Parent component rendered');
 	// The Child component will re-render on every Parent render
 	// because the Parent component is re-rendered on every state change.
 	const [number, setNumber] = useState(0);
+	const [title, setTitle] = useState('React Memo');
 	const onClick = () => {
 		setNumber(Math.random() * 100);
 	};
 
+	const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTitle(e.target.value);
+	};
+
 	return (
 		<>
-			<Child title="React Memo" />
+			{/* <Child title="React Memo" /> */}
+			<MemoizedChild title={title} />
 			<hr></hr>
 			<p>Number: {number}</p>
+
 			<button onClick={onClick}>Click</button>
+			<hr></hr>
+			<label>Title:</label>
+			<input type="text" value={title} onChange={onTitleChange} />
 		</>
 	);
 }
@@ -22,10 +33,14 @@ type ChildProps = {
 	title?: string;
 };
 
+// Memoisation tanımı: Props değişimi olmadan componentin re-render edilmemweini sağlar.
 function Child({ title }: ChildProps) {
 	console.log('Child component rendered');
 	return <>Child: {title}</>;
 }
+
+// React.memo() is a higher order component that memoizes the component.
+const MemoizedChild = React.memo(Child);
 
 function ReactMemo() {
 	return (
