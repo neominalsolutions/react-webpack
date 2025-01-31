00e232fa58067908a8f9edae43234ad5bb80fd90
neominal.solutions <neominal.solutions@gmail.com>
2025-01-31 09:38:40 +0300

    React Memo Sample

--- src/pages/memoisation/pages/react-memo.tsx
+++ src/pages/memoisation/pages/react-memo.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function Parent() {
+	// The Child component will re-render on every Parent render
+	// because the Parent component is re-rendered on every state change.
+	const [number, setNumber] = useState(0);
+	const onClick = () => {
+		setNumber(Math.random() * 100);
+	};
+
+	return (
+		<>
+			<Child title="React Memo" />
+			<hr></hr>
+			<p>Number: {number}</p>
+			<button onClick={onClick}>Click</button>
+		</>
+	);
+}
+
+type ChildProps = {
+	title?: string;
+};
+
+function Child({ title }: ChildProps) {
+	console.log('Child component rendered');
+	return <>Child: {title}</>;
+}
 
 function ReactMemo() {
-	return <>React Memo</>;
+	return (
+		<>
+			<Parent />
+		</>
+	);
 }
 
 export default ReactMemo;
